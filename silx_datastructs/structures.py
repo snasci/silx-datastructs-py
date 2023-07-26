4bcce20b1c7dc4ed07ef1f5baf1b436be1a6dd56
Michael Samsom <mike.samsom@asica.ca>
2023-07-25 18:06:30 -0600

    added PaperIR structure

---
+++ silx_datastructs/structures.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel
+
+
+class Paper(BaseModel):
+    id: int
+    paper_type: str
+    data_source: str
+    n_studies: int
+
+
+class Population(BaseModel):
+    species: str
+    sampling: str
+    n: int | None
+    inclusion: list[str] | None = None
+    exclusion: list[str] | None = None
+
+
+class Entity(BaseModel):
+    index: int
+    token: str
+    var_type: str
+    domain: list[str]
+    unit: str | None
+    context: str
+    measure: str | None
+    distribution: str | None
+
+
+TValue = float | int | bool | dict[str, float]
+
+
+class EntityState(BaseModel):
+    entity: Entity
+    state: str | None
+
+
+class SID(BaseModel):
+    causes: list[EntityState]
+    outcome: EntityState
+
+
+class Datum(BaseModel):
+    n: int | None
+    value: TValue
+    variance: float | tuple[float, float] | None
+    variance_type: str | None
+    sid: SID
+
+
+class PaperIR(BaseModel):
+    paper: Paper
+    population: Population
+    entities: list[Entity]
+    data: list[Datum]
